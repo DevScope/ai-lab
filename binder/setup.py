bc93e0dd0dd1bad61a81a425a5e13d9d6848fbb1
rquintino <rquintino@users.noreply.github.com>
2018-08-24 22:50:27 +0100

    test setup.py in binder

---
+++ binder/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="ailab",
+    version="0.0.1",
+    author="DevScope AI Lab",
+    author_email="",
+    description="DevScope AI Lab",
+    long_description="DevScope AI Lab",
+    long_description_content_type="text/markdown",
+    url="https://github.com/devscope/ai-lab",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
